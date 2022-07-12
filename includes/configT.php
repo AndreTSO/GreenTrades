19cf8d53847a7729b36d23be7447f00f5dcc72f8
kelelawar1 <74416929+kelelawar1@users.noreply.github.com>
2022-07-12 22:21:55 +0000

    Ultimas adiçoes, Backend Emanuel

--- includes/configT.php
+++ includes/configT.php
@@ -11,39 +11,38 @@ define('PARCIAL_ROOT','public_html/');
 define('DOCUMENT_BASE','mysql10.000webhost.com'.PARCIAL_ROOT);
 define('DOCUMENT_ROOT',$_SERVER{'DOCUMENT_ROOT'}.'/'.PARCIAL_ROOT);
 
-
-
-define('DB_SERVER','10.154.0.14:3306');
+define('DB_SERVER','10.154.0.6');
 define('DB_DB','greenTrades');
-define('DB_USER','leoAll');
-define('DB_PASSW','leoAll');
-require_once('connectBD.php');
+define('DB_USER','back1');
+define('DB_PASSW','back1');
+
+require_once('classes/adodb/adodb-errorhandler.inc.php');
+include "connectBD.php";
 /*
-define('DB_SERVER','10.154.0.15:3306');
+define('DB_SERVER','10.154.0.12');
 define('DB_DB','greenTrades');
-define('DB_USER','leoAll');
-define('DB_PASSW','leoAll');
+define('DB_USER','back1');
+define('DB_PASSW','back1');
 
 
-
-//require_once('classes/adodb/adodb-errorhandler.inc.php');
-
-
-
+//require_once('connectBD.php');
 require_once('classes/adodb/adodb.inc.php');
+
 require_once('classes/adodb/adodb-loadbalancer.inc.php');
 
 
+
+
+
 $db = new ADOdbLoadBalancer;
-$db0 = new ADOdbLoadBalancerConnection('mysqli','write',1,false,'10.154.0.14:3306','leoAll','leoAll','greenTrades');
+$db0 = new ADOdbLoadBalancerConnection('mysqli','write',1,false,'10.154.0.06','back1','back1','greenTrades');
 $db->addConnection($db0);
-$db1 = new ADOdbLoadBalancerConnection('mysqli','write',2,false,'10.154.0.15:3306','leoAll','leoAll','greenTrades');
-$db->addConnection($db1);
+//$db1 = new ADOdbLoadBalancerConnection('mysqli','readonly',2,false,'10.154.0.12','back1','back1','greenTrades');
+//$db->addConnection($db1);
+
 $db->setCharset('utf8');
 */
 
-
-session_start();
 
 foreach ($_POST as $key => $input_arr) {
      //  $_POST[$key] = addslashes(trim($input_arr));
--- registWithOauth.php
+++ registWithOauth.php
@@ -1,17 +1,28 @@
 
 <?php
-require_once '/oauth/vendor/autoload.php';
 
-$clientID ="687068431929-2bg3i2cr0tiotdgmooemp77pi9fvcrpn.apps.googleusercontent.com";
-$clientSecret="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";
-$redirect = "https://ptr.emanuenunes.pt/registWithOauth.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
+require_once 'vendor/autoload.php';
+require_once 'vendor/google/auth/autoload.php';
+//require_once 'vendor/Google/Service/Oauth2.php';
+//require_once 'vendor/google/google-api-php-client/autoload.php';
+$clientID ="687068431929-hgirk367sufv87bt0uomcru57de7bdao.apps.googleusercontent.com";
+$clientSecret="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";
+$redirect = "http://greentrades.me/registWithOauth.php";
 
 //creating client Request to google
 
+
 $client = new Google_Client();
+
 $client-> setClientId($clientID);
 $client-> setClientSecret($clientSecret);
-$client-> serRedirectUrl($redirect);
+$client-> setRedirectUri($redirect);
 $client-> addScope('profile');
 $client-> addScope('email');
 
@@ -19,16 +30,25 @@ $client-> addScope('email');
 
 
 if (isset($_GET['code'])){
+    //echo "LOguei mas agora faço o que ?<br>";
+    //echo "<br>Token-> ".$_GET['code']."<br>";
 
-    $token= $client-> fetchAccessToken($_GET['code']);
+    //$client->authenticate($_GET['code']);
+   // $token = $client->getAccessToken();
+    //var_dump($token);
+
+   // $token= $client-> fetchAccessToken($_GET['code']);
+    $token= $client-> fetchAccessTokenWithAuthCode($_GET['code']);
     $client->setAccessToken($token);
-
-
+  
+    
     //GETing use datta com pronucia indiana
-    $gauth = new Google_Service_Oauth($client);
+    $gauth = new Google_Service_Oauth2($client);
     $google_info = $gauth->userinfo->get();
+    //echo "zona3";
     $email = $google_info->email;
     $name =  $google_info->name;
+    //echo "zona4";
 
     echo "Welcomme ".$name." email: ".$email;
 
